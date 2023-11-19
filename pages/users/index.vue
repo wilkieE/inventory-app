62d058232935e927a37f0656e9145af017696d1b
Ejiro Wilkie <wilkieejiroghene@gmail.com>
2023-11-19 14:13:39 +0100

    add crud functionality to users page

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -2,8 +2,11 @@
   <div class="container mx-auto p-4">
     <h1 class="text-2xl font-bold mb-4">User Management</h1>
     <div class="mb-4">
-      <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" @click="addNewUser">Add New User</button>
+      <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" @click="showAddUserModal">Add New
+        User</button>
     </div>
+
+    <!-- User List -->
     <table class="min-w-full bg-white">
       <thead>
         <tr>
@@ -28,11 +31,33 @@
           <td class="px-4 py-2 border-b border-gray-200">{{ user.department }}</td>
           <td class="px-4 py-2 border-b border-gray-200">
             <button class="text-blue-600 hover:text-blue-800" @click="editUser(user)">Edit</button>
-            <button class="text-red-600 hover:text-red-800 ml-2" @click="deleteUser(user.id)">Delete</button>
+            <button class="text-red-600 hover:text-red-800 ml-2" @click="confirmDeleteUser(user.id)">Delete</button>
           </td>
         </tr>
       </tbody>
     </table>
+
+    <!-- Add/Edit User Modal -->
+    <div v-if="showModal" class="modal">
+      <div class="modal-content">
+        <h2 class="text-xl font-bold mb-4">{{ editingUser ? 'Edit User' : 'Add New User' }}</h2>
+        <label>Name: <input v-model="modalData.name" type="text"></label>
+        <label>Email: <input v-model="modalData.email" type="email"></label>
+        <label>Department: <input v-model="modalData.department" type="text"></label>
+        <button @click="handleModalSubmit">{{ editingUser ? 'Update' : 'Add' }}</button>
+        <button @click="closeModal">Cancel</button>
+      </div>
+    </div>
+  </div>
+
+  <!-- Confirmation Dialog -->
+  <div v-if="showConfirmationDialog" class="confirmation-dialog">
+    <div class="confirmation-content">
+      <h2>Confirm Deletion</h2>
+      <p>Are you sure you want to delete this user?</p>
+      <button @click="confirmDeletion">Yes, Delete</button>
+      <button @click="cancelDeletion">Cancel</button>
+    </div>
   </div>
 </template>
 
@@ -42,15 +67,107 @@ import { useFetch } from '#app';
 
 const { data: users, pending, refresh, error } = await useFetch('/api/users');
 
-//TODO add crud logic
-const addNewUser = () => {
+const showModal = ref(false);
+const editingUser = ref(null);
+const modalData = ref({ name: '', email: '', department: '' });
+
+const showConfirmationDialog = ref(false);
+let userToDelete = ref(null);
+
+const confirmDeleteUser = (userId) => {
+  showConfirmationDialog.value = true;
+  userToDelete.value = userId;
+};
+
+const confirmDeletion = async () => {
+  await deleteUser(userToDelete.value);
+  showConfirmationDialog.value = false;
+};
+
+const cancelDeletion = () => {
+  showConfirmationDialog.value = false;
+};
+
+
+const showAddUserModal = () => {
+  editingUser.value = null;
+  modalData.value = { name: '', email: '', department: '' };
+  showModal.value = true;
 };
 
 const editUser = (user) => {
+  editingUser.value = user;
+  modalData.value = { ...user };
+  showModal.value = true;
+};
+
+const handleModalSubmit = async () => {
+  if (editingUser.value) {
+    await updateUser(editingUser.value.id, modalData.value);
+  } else {
+    await addNewUser(modalData.value);
+  }
+  closeModal();
+};
+
+const closeModal = () => {
+  showModal.value = false;
+};
+
+const addNewUser = async (newUser) => {
+  try {
+    const response = await fetch('api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    });
+
+    if (response.ok) {
+      refresh();
+    } else {
+      console.error('Error adding user:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+const updateUser = async (userId, updatedUser) => {
+  try {
+    const response = await fetch(`api/users/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedUser),
+    });
+
+    if (response.ok) {
+      refresh();
+    } else {
+      console.error('Error updating user:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
 
 const deleteUser = async (userId) => {
-  refresh();
-};
+  try {
+    const response = await fetch(`api/users/${userId}`, {
+      method: 'DELETE'
+    });
 
+    if (response.ok) {
+      refresh();
+    } else {
+      console.error('Error deleting user:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 </script>
+
