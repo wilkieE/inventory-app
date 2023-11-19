8acb9a0e038e861a31664664f7280654405e9ac5
Ejiro Wilkie <wilkieejiroghene@gmail.com>
2023-11-19 20:08:46 +0100

    update return page

--- pages/return.vue
+++ pages/return.vue
@@ -8,7 +8,7 @@
                     <label for="item" class="block text-gray-700 text-sm font-bold mb-2">Select Item to Return:</label>
                     <select id="item" v-model="selectedItem"
                         class="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                        <option v-for="item in items" :key="item.id" :value="item.id">{{ item.name }}</option>
+                        <option v-for="item in filteredItems" :key="item.id" :value="item.id">{{ item.name }}</option>
                     </select>
                 </div>
                 <div class="mb-4">
@@ -25,21 +25,50 @@
     </div>
 </template>
   
-<script setup>
-import { ref } from 'vue';
+<script setup lang="ts">
+import { ref, computed, type ComputedRef } from 'vue';
 import { useFetch } from '#app';
 
-//TODO filter items to only show items that are checked out
-const { data: items, pending } = await useFetch('/api/items');
-const selectedItem = ref('');
-const returnNotes = ref('');
+interface Item {
+    id: number;
+    name: string;
+    status: string;
+}
 
-const returnItem = async () => {
+const selectedItem = ref<number | string>('');
+const returnNotes = ref<string>('');
+const { data: items, pending, refresh } = await useFetch<Item[]>('/api/items');
+
+const filteredItems: ComputedRef<Item[]> = computed(() => {
+    return items.value?.filter(item => item.status === 'in use') || [];
+
+});
+
+const returnItem = async (): Promise<void> => {
     if (!selectedItem.value) {
         alert('Please select an item to return.');
         return;
     }
 
-    // TODO implement return logic
+    try {
+        const response = await fetch('/api/items/return', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ itemId: selectedItem.value, returnNotes: returnNotes.value })
+        });
+
+        if (response.ok) {
+            alert('Item returned successfully!');
+            selectedItem.value = '';
+            returnNotes.value = '';
+            await refresh();
+        } else {
+            throw new Error('Failed to return item');
+        }
+    } catch (error: any) {
+        console.error('Error returning item:', error);
+        alert('Failed to return item.');
+    }
 };
-</script> 
+</script>
+  
