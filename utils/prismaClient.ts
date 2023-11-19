b468757fa2b609e223474c6f40bac9e32e43aa42
Ejiro Wilkie <wilkieejiroghene@gmail.com>
2023-11-19 11:44:46 +0100

    update: soft delete logic because prisma.use is deprecated

--- utils/prismaClient.ts
+++ utils/prismaClient.ts
@@ -1,20 +1,27 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-prisma.$use(async (params, next) => {
-  if (params.model && params.action === "delete") {
-    params.action = "update";
-    params.args["data"] = { deletedAt: new Date() };
-  } else if (params.model && params.action === "deleteMany") {
-    params.action = "updateMany";
-    if (!params.args.data) {
-      params.args.data = {};
-    }
-    params.args.data.deletedAt = new Date();
-  }
+async function softDeleteUser(args: Prisma.UserWhereUniqueInput) {
+  await prisma.user.update({
+    where: { ...args },
+    data: { deletedAt: new Date() },
+  });
+}
 
-  return next(params);
-});
+async function softDeleteItem(args: Prisma.ItemWhereUniqueInput) {
+  await prisma.item.update({
+    where: { ...args },
+    data: { deletedAt: new Date() },
+  });
+}
+
+async function softDeleteUsageLog(args: Prisma.UsageLogWhereUniqueInput) {
+  await prisma.usageLog.update({
+    where: { ...args },
+    data: { deletedAt: new Date() },
+  });
+}
 
 export default prisma;
+export { softDeleteUser, softDeleteItem, softDeleteUsageLog };
